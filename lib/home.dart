1919430d9230ee0ea3d9d625f5aa09de6465a2e4
Franklin Da Silva Martins <gentilezaxd@gmail.com>
2023-07-12 19:27:19 -0400

    Remove _buildGirdCard function

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:shrine/model/product.dart';
 import 'package:shrine/model/products_repository.dart';
 import 'package:shrine/supplemental/asymmetric_view.dart';
@@ -10,59 +9,6 @@ class HomePage extends StatelessWidget {
     this.category = Category.all,
     Key? key
   }):super(key: key);
-
-  List<Card> _buildGridCards(BuildContext context){
-    List<Product> products = ProductsRepository.loadProducts(Category.all);
-
-    if(products.isEmpty){
-      return const <Card>[];
-    }
-
-    final ThemeData theme = Theme.of(context);
-    final NumberFormat formatter = NumberFormat.simpleCurrency(
-      locale: Localizations.localeOf(context).toString()
-    );
-
-    return products.map((product){
-      return Card(
-        clipBehavior: Clip.antiAlias,
-        child: Column(
-
-          children: [
-            AspectRatio(
-              aspectRatio: 18/11,
-              child: Image.asset(
-                product.assetName,
-                package: product.assetPackage,
-                fit: BoxFit.fitWidth,
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
-                      product.name,
-                      style: theme.textTheme.titleLarge,
-                      maxLines: 1,
-                    ),
-                    const SizedBox(height: 8.0,),
-                    Text(
-                      formatter.format(product.price),
-                      style: theme.textTheme.titleSmall,
-                    )
-                  ]
-                ),
-              ),
-            )
-          ]
-        ),
-      );
-    }).toList();
-  }
 
   @override
   Widget build(BuildContext context) {
